c22b75571e0693f19aa27e0df7ddb1f6f2366270
Kyosuke Miyazawa <31465059+zenmai36rai@users.noreply.github.com>
2022-11-16 13:29:19 +0900

    Create Project And Add Some Unit

---
+++ ShootingBasic/ShootingBasic/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+End Class
